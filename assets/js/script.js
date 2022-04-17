75a32a212d8e386078c436750ceed33d9c54823e
maggiejoe <maggiejoseph94@gmail.com>
2022-04-17 15:03:26 -0400

    js file started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,29 @@
+// current date & time
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
+var taskList = {
+    "9": [],
+    "10": [],
+    "11": [],
+    "12": [],
+    "13": [],
+    "14": [],
+    "15": [],
+    "16": [],
+    "17": []
+}
+
+// adding tasks to local storage
+var savedTask = function() {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+}
+
+// load tasks into their proper container
+var loadTask = function() {
+    var loadedTask = JSON.parse(localStorage.getItem("taskList"));
+    if (loadedTask) {
+        taskList = loadedTask
+
+    }
+
+}
